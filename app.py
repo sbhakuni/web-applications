d0267f3f0f5d7c96a4ed7a0bff1c1f81616fa61c
sbhakuni <bhakunishaantanu@gmail.com>
2024-03-29 00:31:51 -0700

    joblist, home page

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from helper import JOBS
 
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def hello_company():
-    return render_template('home.html',company_name="Test Company")
+    return render_template('home.html',company_name="Test Company",jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
---
+++ helper.py
@@ -0,0 +1,25 @@
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'New York City, NY, USA',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, CA, USA',
+    'salary': '$150,000'
+  }
+]
